46760a17ab647ff427a262e5c89c7cabe01cdac5
Marcus.J <airsize@gmail.com>
2018-08-20 21:12:54 +1200

    Finish iteration #3

--- Iteration 3/4. PROGRAM/spec/Guesser1Spec.js
+++ Iteration 3/4. PROGRAM/spec/Guesser1Spec.js
@@ -1,5 +1,5 @@
 describe("guesser1", () => {
-  let theGuesser1 = controller.guesser1;
+  let theGuesser1 = new Vue(viewModel).guesser;
   theGuesser1.setup();
   let numberInput = document.getElementById("numberInput");
 
@@ -8,32 +8,53 @@ describe("guesser1", () => {
     expect(theNumber).toBeTruthy(0 <= theNumber && theNumber < 100);
   });
 
+  describe("guesser1.inputNumber", () => {
+    it(`should have the inputNumber equal 99 if the input is 999`, () => {
+      theGuesser1.inputNumber = 999;
+      expect(theGuesser1.inputNumber).toEqual(99);
+    });
+
+    it(`should have the inputNumber equal 0 if the input is -999`, () => {
+      theGuesser1.inputNumber = -999;
+      expect(theGuesser1.inputNumber).toEqual(0);
+    });
+  });
+
   describe(`guesser1.compareNumber`, () => {
     it(`should return "Try lower"`, () => {
       theGuesser1.actualNumber = 51;
-      expect(theGuesser1.compareNumber(52)).toEqual(`Try lower`);
+      theGuesser1._inputNumber = 52;
+      expect(theGuesser1.compareNumber()).toEqual(`Try lower`);
     });
 
     it(`should return "Try higher"`, () => {
       theGuesser1.actualNumber = 51;
-      expect(theGuesser1.compareNumber(49)).toEqual(`Try higher`);
+      theGuesser1._inputNumber = 49;
+      expect(theGuesser1.compareNumber()).toEqual(`Try higher`);
     });
     it(`should return "You got it in 7 trials"`, () => {
       theGuesser1.setup();
       theGuesser1.actualNumber = 0;
-      theGuesser1.compareNumber(50);
+      theGuesser1._inputNumber = 50;
+      theGuesser1.compareNumber();
 
-      theGuesser1.compareNumber(25);
+      theGuesser1._inputNumber = 25;
+      theGuesser1.compareNumber();
 
-      theGuesser1.compareNumber(12);
+      theGuesser1._inputNumber = 12;
+      theGuesser1.compareNumber();
 
-      theGuesser1.compareNumber(6);
+      theGuesser1._inputNumber = 6;
+      theGuesser1.compareNumber();
 
-      theGuesser1.compareNumber(3);
+      theGuesser1._inputNumber = 3;
+      theGuesser1.compareNumber();
 
-      theGuesser1.compareNumber(1);
+      theGuesser1._inputNumber = 1;
+      theGuesser1.compareNumber();
 
-      expect(theGuesser1.compareNumber(0)).toEqual(`You got it in 7 trials`);
+      theGuesser1._inputNumber = 0;
+      expect(theGuesser1.compareNumber()).toEqual(`You got it in 7 trials`);
     });
   });
 });
--- Iteration 3/4. PROGRAM/src/Guesser1.js
+++ Iteration 3/4. PROGRAM/src/Guesser1.js
@@ -1,8 +1,21 @@
-class Guesser1 {
+
+class Guesser1 { // eslint-disable-line no-unused-vars
   constructor() {
-    this.inputNumber= null;
-    this.actualNumber=null;
+    this._inputNumber = null;
+    this.actualNumber = null;
     this.counter = 0;
+  }
+
+  get inputNumber() {
+    return this._inputNumber;
+  }
+
+  set inputNumber(input) {
+    if (input < 0) {
+      this._inputNumber = 0;
+    } else if (99 < input) {
+      this._inputNumber = 99;
+    } else this._inputNumber = input;
   }
 
   getActualNumber() {
@@ -10,23 +23,30 @@ class Guesser1 {
   }
 
   setup() {
+    document.getElementById("numberInputBox").value = null;
+    //document.getElementById("numInputBtn").removeAttribute("disabled");
+    this._inputNumber = null;
     this.counter = 0;
     this.actualNumber = Math.floor(Math.random() * 100);
   }
 
-  compareNumber(inputNumber) {
+  compareNumber() {
     var message = "";
-    this.inputNumber = inputNumber;
     this.counter++;
-    if (this.inputNumber === this.actualNumber) {
+    if (this._inputNumber === this.actualNumber) {
       message = `You got it in ${this.counter} trials`;
-      document.getElementById("numInputBtn").setAttribute("disabled", "disabled");
+      //document.getElementById("numInputBtn").setAttribute("disabled", "disabled");
       // document.getElementById("numberInput").setAttribute("disabled", "disabled");
-    } else if (this.inputNumber > this.actualNumber) {
+    } else if (this._inputNumber > this.actualNumber) {
       message = `Try lower`;
-    } else if (this.inputNumber < this.actualNumber) {
+    } else if (this._inputNumber < this.actualNumber) {
       message = `Try higher`;
     }
     return message;
+  }
+   setInputNumber() {
+    this.inputNumber = parseInt(
+      document.getElementById("numberInputBox").value
+    );
   }
 }
